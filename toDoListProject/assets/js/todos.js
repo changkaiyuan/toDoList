7da00d9e1e7299f33753312c19a30f1db7843985
Changkai Yuan <56485669+changkaiyuan@users.noreply.github.com>
2020-02-19 10:16:05 -0800

    Add files via upload
    This is a todo list project.
    you can add, delete and mark off an item

--- toDoListProject/assets/js/todos.js
+++ toDoListProject/assets/js/todos.js
@@ -1,35 +1,12 @@
 //mark off finished todos by clicking
 //
-//method 1
-
-
-// $('li').click(function(){
-// 	// turn black if gray
-// 	if($(this).css("color") === "rgb(128, 128, 128)")
-// 	{
-// 		$(this).css({
-// 			color:"black",
-// 			textDecoration:"none"
-// 		});
-// 	}
-// 	//else turn gray
-// 	else{
-// 		$(this).css({
-// 		color:"gray",
-// 		textDecoration:"line-through"
-// 	});
-// 	}
-
-// });
-// 
-// method 2
 
 $("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 })
 
 
-// click x to delete todo
+// click to delete todo
 $("ul").on("click", "span",function(event){
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
@@ -40,10 +17,14 @@ $("ul").on("click", "span",function(event){
 
 //add todo after hitting enter
 $("input[type='text']").keypress(function(event){
-	if(event.which===13){
+	if(event.which===13 &&$(this).val()!=""){
 		var todoText = $(this).val();
-		$("ul").append("<li><span>x</span> " + todoText + "</li>");
+		$("ul").append("<li><span><i class=\"fa fa-trash\"></i></i></span> " + todoText + "</li>");
 		$(this).val("");
 
 	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
 });
